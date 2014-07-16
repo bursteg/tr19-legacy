b8b3e2c13b0ca5a7d13c0eaeef7a701414075724
Guy Burstein <guyb@microsoft.com>
2014-07-16 13:57:10 -0700

    twittersearch.web

---
+++ TwitterSearch.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TwitterSearch.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ TwitterSearch.Web/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+﻿using Legacy.Common.Models;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TwitterSearch.Web.Controllers
+{
+  public class SearchController : ApiController
+  {
+    [HttpPost]
+    [Route("search")]
+    public void HandleNotification(HttpRequestMessage request)
+    {
+      SearchRequest payload = request.Content.ReadAsAsync<SearchRequest>().Result;
+
+      var connString = ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString;
+      var storageAccount = CloudStorageAccount.Parse(connString);
+
+      var queueClient = storageAccount.CreateCloudQueueClient();
+      var queue = queueClient.GetQueueReference("search-queue");
+      queue.CreateIfNotExists();
+
+      var serialized = JsonConvert.SerializeObject(payload);
+      var message = new CloudQueueMessage(serialized);
+      queue.AddMessage(message);
+    }
+  }
+}
